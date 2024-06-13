68aacdca2220990172c3d34590d674e774e280d9
Shangri <shangrialena@gmail.com>
2024-06-13 13:54:27 +0200

    fixed slide5

--- js/packages-modules.js
+++ js/packages-modules.js
@@ -62,19 +62,6 @@ function buildSubscriptionModulesPanel(){
         });
 
         subscriptionModulesPanelHtml += `
-                                <div data-package-modal-close class="package-modal-close lg:hidden">
-                                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                <g clip-path="url(#clip0_1_1170)">
-                                                                    <path d="M18 6L6 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                    <path d="M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                </g>
-                                                                <defs>
-                                                                    <clipPath id="clip0_1_1170">
-                                                                        <rect width="24" height="24" fill="currentColor"></rect>
-                                                                    </clipPath>
-                                                                </defs>
-                                                            </svg>
-                                                        </div>
                                      <div class="dropdown-box closing" data-tab-item="${service.name}">
                                           <div class="toggle-container">
                                                 <button class="dropdown-toggle">
